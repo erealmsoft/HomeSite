ac24a331a3a996e3d086ce10ae3ec8ccad65de8a
dangjian <hunterdang@gmail.com>
2014-08-04 11:30:24 +0800

    1.config 2. compress image.

--- gruntfile.js
+++ gruntfile.js
@@ -26,6 +26,10 @@ module.exports = function(grunt){
             main: {
                 files: [
                     {
+                        expand: true, cwd: 'public/vendor/jQuery/dist',
+                        src: ['jquery.min.map','jquery.min.js'], dest: "public/javascripts/libs"
+                    },
+                    {
                         expand: true, cwd: 'public/vendor/angular',
                         src: ['angular.min.js.map','angular.min.js'], dest: "public/javascripts/libs"
                     },
@@ -38,10 +42,43 @@ module.exports = function(grunt){
                         src: ['ui-bootstrap.min.js'], dest: "public/javascripts/libs/plugins"
                     },
                     {
-                        expand: true, cwd: 'public/vendor/jQuery/dist',
-                        src: ['jquery.min.map','jquery.min.js'], dest: "public/javascripts/libs"
+                        expand: true, cwd: 'public/vendor/font-awesome/fonts',
+                        src: ['**'], dest: "public/stylesheets/fonts"
                     },
+                    {
+                        expand: true, cwd: 'public/vendor/bootstrap/fonts',
+                        src: ['**'], dest: "public/stylesheets/fonts"
+                    }
                 ]
+            },
+            images: {
+                files: [
+                    // copy minified images
+                    {expand: true, cwd: 'public/images-build', src: ['**'], dest: 'public/images'}
+                ]
+            },
+            build: {
+                files: [
+                    // copy minified images
+                    {expand: true, cwd: 'public/stylesheets/fonts', src: ['**'], dest: 'public/fonts'}
+                ]
+            }
+        },
+        clean: {
+            images: {
+                src: ['public/images-build']
+            },
+            build: {
+                src: [
+                    'build',
+                    'public/images-build', 'public/stylesheets/**/*', "!public/stylesheets/*.min.css",
+                    'public/javascripts/app','public/javascripts/*.js', 'public/javascripts/debug', 'public/javascripts/libs', "!public/javascripts/*.min.js"]
+            },
+            web: {
+                src: ['app/controllers/management.java', 'app/controllers/report.java']
+            },
+            api: {
+                src: ['app/controllers/Application.java', 'app/controllers/Secured.java']
             }
         },
         jshint: {
@@ -67,6 +104,29 @@ module.exports = function(grunt){
                     'config/**/*.js',
                     'app/**/*.js'
                 ]
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'public/images',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'public/images-build/'
+                }]
+            }
+        },
+        cssmin: {
+            build: {
+                options: {
+                },
+                files: {
+                    'public/stylesheets/libs.min.css':
+                        [
+                            "public/stylesheets/libs/bootstrap.min.css",
+                            "public/stylesheets/libs/reset.css"
+                        ]
+                }
             }
         },
         less: {
@@ -168,6 +228,9 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -181,7 +244,7 @@ module.exports = function(grunt){
     //Making grunt default to force in order not to break the project.
     grunt.option('force', true);
 
-    grunt.registerTask('prepare', ['bower', 'lesslint', 'jshint', 'imagemin', 'copy:images', 'clean:images']);
-    grunt.registerTask('default', ['nodemon:dev','watch','open:dev']);
+    grunt.registerTask('prepare', ['bower', 'copy:main', 'imagemin', 'copy:images', 'clean:images']);
+    grunt.registerTask('default', ['lesslint', 'jshint','nodemon:dev','watch','open:dev']);
     grunt.registerTask('build', ['cssmin', 'less', 'uglify','usemin', 'copy:build', 'clean:build']);
 };
