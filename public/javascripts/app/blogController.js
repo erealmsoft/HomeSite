09b09e8592671ac9b04d4a9faad110dd6405d5f9
dangjian <hunterdang@gmail.com>
2014-10-03 12:04:02 +0800

    change blog module.

--- public/javascripts/app/blogController.js
+++ public/javascripts/app/blogController.js
@@ -9,13 +9,25 @@ angular.module('erealm').controller('BlogController', ['$scope','$sce','$filter'
     'use strict';
 
     angular.extend($scope,{subTitle: "BLOGS", mainTitle: "what we did", currentPage: "blog-page"});
+    var mockDates = [
+        '2014-05-03 11:17:00 GMT',
+        '2014-06-17 18:31:00 GMT',
+        '2014-07-01 13:03:00 GMT',
+        '2014-08-22 18:21:00 GMT',
+        '2014-09-30 14:29:00 GMT'
+    ];
     $scope.loadData = function(language) {
         if (!language){
             language = $translate.uses();
         }
 
         client.getPosts(language).then(function(response){
-            $scope.blogs = response.data;
+            var blogs = response.data
+            var len = blogs.length;
+            for(var i=0;i<len && i < mockDates.length; i++) {
+                blogs[len-i-1].date = mockDates[i];
+            }
+            $scope.blogs = blogs;
         });
     };
     $scope.loadData();
@@ -31,6 +43,6 @@ angular.module('erealm').controller('BlogController', ['$scope','$sce','$filter'
         } else {
             moment.lang('en');
         }
-        return moment(date, 'yyyy-mm-dd').format('lll')
+        return moment(date, 'YYYY-MM-DD HH:mm:ss').format('lll')
     };
 }]);
